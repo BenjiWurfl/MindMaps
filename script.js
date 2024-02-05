3353c8178cbbceec1c54b4bdf1e1eda08a21c721
Benjamin Würflinger <benjamin.wuerflinger@htl-wels.at>
2024-02-05 19:16:59 +0100

    initialize general

--- script.js
+++ script.js
@@ -29,6 +29,7 @@ function redirectToLogin() {
 onAuthStateChanged(auth, (user) => {
     if (user) {
         console.log("User is signed in with UID:", user.uid);
+        initializeMindWired();
         showMindMapListPage();
     } else {
         console.log("No user is signed in.");
@@ -68,7 +69,6 @@ function updateMindMapListUI() {
 document.getElementById('create-new-mindmap').addEventListener('click', async () => {
     const mindMapName = prompt("Bitte geben Sie den Namen der neuen MindMap ein:");
     if (mindMapName && auth.currentUser) {
-        // Rufen Sie die Funktion auf, um die Default-MindMap-Struktur zu erhalten
         const defaultMindMapData = initializeDefaultMindMap();
         const mindMapData = {
             name: mindMapName,
@@ -79,7 +79,6 @@ document.getElementById('create-new-mindmap').addEventListener('click', async ()
             const docRef = await addDoc(collection(db, "users", auth.currentUser.uid, "mindmaps"), mindMapData);
             console.log("Neue MindMap erstellt mit ID:", docRef.id);
             currentMindMapId = docRef.id;
-            // Leiten Sie den Benutzer direkt zum Editor mit den gerade erstellten Daten um
             //initializeMindWired();
             let isNewMindMap = true;
             showMindMapEditorPage(mindMapData, isNewMindMap);
@@ -93,7 +92,7 @@ function navigateToMindMap(mindMapId) {
     const selectedMindMap = mindMaps.find(map => map.id === mindMapId);
     if (selectedMindMap) {
         currentMindMapId = selectedMindMap.id;
-        initializeMindWired();
+        //initializeMindWired();
 
         if (currentMindMapId) {
             loadMindMapFromFirestore(currentMindMapId);
